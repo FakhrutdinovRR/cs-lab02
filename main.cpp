ef4c34a190d9266eb4d2c173270f9d1e2464edc1
Alice (FakhrutdinovRR) <FakhrutdinovRR@mpei.ru>
2022-03-21 02:01:01 +0300

    code: вывод макс значения

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << max(a, b);
 
     return 0;
 }
